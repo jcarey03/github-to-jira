07a397970422a3e1c4f5b851fd4845c8ed8ee5ae
Jason Carey <jcarey@lyft.com>
2022-10-19 13:41:02 -0600

    added requirements and cleanup

--- github_to_jira.py
+++ github_to_jira.py
@@ -1,29 +1,48 @@
 #!/usr/bin/env python
 
-import base64
-import collections
+"""
+Adapted from https://github.com/cnorthwood/github-to-jira.
+"""
+
 import csv
+import itertools
 import itertools as it
 import os
+import subprocess
 import sys
-from time import sleep
 import urllib2
+from time import sleep
 
-from dateutil.parser import parse as dateparse
 import simplejson
+from dateutil.parser import parse as dateparse
 
-GITHUB_API = 'https://github.com/api/v2/json/%s'
-GITHUB_ISSUES_LIST = "issues/list/%s/%s"
-GITHUB_ISSUES_COMMENTS = "issues/comments/%s/%s"
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+GITHUB_REPO = os.getenv('GITHUB_REPO')
+GITHUB_API = 'https://api.github.com/repos/' + GITHUB_REPO
+GITHUB_ISSUES_LIST = GITHUB_API + "/issues"
+GITHUB_ISSUES_COMMENTS = GITHUB_ISSUES_LIST + "/%s/comments"
 
-def github_open_api(call,
-                   username=os.getenv('GITHUB_USERNAME'),
-                   password=os.getenv('GITHUB_PASSWORD')):
-    request = urllib2.Request(GITHUB_API % (call,))
-    if username:
-        auth = base64.encodestring(':'.join((username, password))).strip('\n')
-        request.add_header('Authorization', 'Basic ' + auth)
+# e.g., '[bot]'
+# issues referencing these user prefixes will be filtered out
+ignore_user_prefixes = {}
+
+# e.g., 'jcarey03'
+# issues referencing these users will be filtered out
+inactive_github_users = {
+}
+
+# e.g., 'jcarey03': 'jcarey'
+# mapping of github username to jira username
+# any user that is not in inactive_github_users must have a mapping
+github_user_to_jira_user = {
+}
+
+
+def github_open_api(call):
+    request = urllib2.Request(call)
+    request.add_header('Authorization', 'token ' + GITHUB_TOKEN)
     return urllib2.urlopen(request)
+
 
 def github_api_call(call):
     """
@@ -34,54 +53,97 @@ def github_api_call(call):
     except urllib2.HTTPError as e:
         if e.code == 403:
             # hit the rate limit - wait 60 seconds then retry
-            print >>sys.stderr, "Hit the rate limit, waiting 60 seconds..."
+            print >> sys.stderr, "Hit the rate limit, waiting 60 seconds..."
             sleep(60)
             return github_api_call(call)
         else:
             raise
 
+
 def get_num_comments(issue):
-    return issue['comments']
+    return issue['_comments']
+
 
 def get_num_labels(issue):
-    return len(issue['labels'])
+    return len(issue['Labels'])
 
-def get_comments(repository, issue):
-    """
-    Get a list of all the comments for this issue as dictionaries.
-    """
-    print "Fetching comments for issue %d..." % issue['number']
+
+def get_num_watchers(issue):
+    return len(issue['Watchers'])
+
+
+def get_comments(issue):
     comments = []
-    data = github_api_call(GITHUB_ISSUES_COMMENTS %
-                                               (repository, issue['number']))
-    for comment in data['comments']:
-        comments.append({
-            'created_at': dateparse(comment['created_at']),
-            'body': comment['body'],
-        })
+    data = github_api_call(GITHUB_ISSUES_COMMENTS % issue['_id'])
+    if len(data) > 0:
+        for comment in data:
+            if is_valid_user(comment['user']):
+                comment_header = format_datetime(dateparse(comment['created_at'])) + ';' + \
+                                 github_user_to_jira_user[comment['user']['login']] + ';'
+                comments.append({
+                    'Comment Body': comment_header + convert_markdown(comment['body']),
+                })
     return comments
 
-def get_labels(repository, issue):
-    """
-    Get a list of all the labels associated with this issue.
-    """
-    return issue['labels']
 
-def load_github_issues(repository):
-    """
-    Get all the issues associated with a Github repository as a dictionary of
-    issues, where the key is the issue ID and the value is a dictionary of the
-    issue keys: created_at, state, title, body, comments, which is a list of
-    dictionaries with keys: created_at and body
-    """
+def is_valid_user(user):
+    return not (
+        any([prefix in user['login'] for prefix in ignore_user_prefixes]) or
+        user['login'] in inactive_github_users
+    )
+
+
+def get_labels(issue):
+    return issue['Labels']
+
+
+def get_assignee(user):
+    if user and is_valid_user(user):
+        return github_user_to_jira_user[user['login']]
+
+
+def get_watchers(assignees):
+    watchers = []
+    for assignee in assignees or []:
+        if is_valid_user(assignee):
+            watchers.append(github_user_to_jira_user[assignee['login']])
+    return watchers
+
+
+def load_github_issues():
     issues = {}
-    for state in ('open', 'closed'):
-        data = github_api_call(GITHUB_ISSUES_LIST % (repository, state))
-        print "Fetched %s issues" % state
-        for issue in data['issues']:
-            issue['created_at'] = dateparse(issue['created_at'])
-            issues[issue['number']] = issue
+    for page_idx in range(1, 10000):
+        api_call = GITHUB_ISSUES_LIST + '?per_page=100&page=%s&state=all' % page_idx
+        print "Fetching page %d..." % page_idx
+        data = github_api_call(api_call)
+        if len(data) == 0:
+            print('Breaking on page %d' % page_idx)
+            break
+        for issue in data:
+            if is_valid_user(issue['user']) and 'pull_request' not in issue:
+                issue_id = '[%s|%s]' % (issue['html_url'].split('/')[-1], issue['html_url'])
+                issues[issue_id] = {
+                    'ID': issue_id,
+                    'Summary': issue['title'],
+                    'Description': convert_markdown(issue['body']),
+                    'Labels': map(lambda x: x['name'].replace(' ', '_'), issue['labels'] or []),
+                    'Reporter': github_user_to_jira_user[issue['user']['login']],
+                    'Assignee': get_assignee(issue['assignee']),
+                    'Watchers': get_watchers(issue['assignees']),
+                    'Date Created': format_datetime(dateparse(issue['created_at'])),
+                    'Date Modified': format_datetime(dateparse(issue['updated_at'])),
+                    'Date Resolved': format_datetime(dateparse(issue['closed_at'])) if issue.get('closed_at') else None,
+                    'Status': 'Closed' if issue.get('closed_at') else 'Open',
+                    'Resolution': 'Done' if issue.get('closed_at') else 'Unresolved',
+                    '_comments': issue['comments'],
+                    '_id': issue['number'],
+                }
     return issues
+
+
+def format_datetime(dt):
+    return dt.strftime('%Y/%m/%d %H:%M')
+
 
 def ensure_encoded(obj, encoding='us-ascii'):
     """
@@ -91,6 +153,7 @@ def ensure_encoded(obj, encoding='us-ascii'):
         return obj.encode(encoding)
     else:
         return obj
+
 
 def pad_list(l, size, obj):
     """
@@ -107,32 +170,66 @@ def pad_list(l, size, obj):
     """
     return list(it.islice(it.chain(l, it.repeat(obj)), size))
 
-def write_jira_csv(fd, repository):
-    # Get the most comments on an issue to decide how many comment columns we
-    # need
-    issues = load_github_issues(repository).values()
+
+def convert_markdown(gh_markdown):
+    if gh_markdown:
+        p = subprocess.Popen(
+            [
+                '/Users/jcarey/.pyenv/shims/python',
+                '/Users/jcarey/src/mistletoe/mistletoe/main_converter.py'
+            ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env={'PYTHONPATH': '/Users/jcarey/src/mistletoe'},
+        )
+        output, err = p.communicate(gh_markdown.encode('utf-8'))
+        if err:
+            raise Exception('Failed markdown conversion: %s' % err)
+        return output.rstrip()
+
+
+def write_jira_csv(fd):
+    issues = load_github_issues().values()
     issue_writer = csv.writer(fd)
     max_num_labels = max(map(get_num_labels, issues))
+    max_num_watchers = max(map(get_num_watchers, issues))
     max_num_comments = max(map(get_num_comments, issues))
-    label_headers = ['Labels %d' % (i+1) for i in xrange(max_num_labels)]
-    comment_headers = ['Comments %d' % (i+1) for i in xrange(max_num_comments)]
-    headers = ['ID', 'Title', 'Body', 'Created At', 'State']
+    label_headers = ['Labels' for _ in xrange(max_num_labels)]
+    watcher_headers = ['Watchers' for _ in xrange(max_num_watchers)]
+    comment_headers = ['Comment Body' for _ in xrange(max_num_comments)]
+    single_field_headers = [
+        'ID',
+        'Summary',
+        'Description',
+        'Reporter',
+        'Assignee',
+        'Date Created',
+        'Date Modified',
+        'Date Resolved',
+        'Status',
+        'Resolution'
+    ]
+    headers = []
+    headers += single_field_headers
     headers += label_headers
+    headers += watcher_headers
     headers += comment_headers
     issue_writer.writerow(headers)
-    known_attr_parsers = dict(
-            created_at=lambda x: x.strftime('%Y/%m/%d %H:%M'))
-    all_attr_parsers = collections.defaultdict(lambda: lambda x: x,
-                                               known_attr_parsers)
+    issue_idx = 1
     for issue in issues:
-        attrs = ['number', 'title', 'body', 'created_at', 'state']
-        row = [all_attr_parsers[attr](issue[attr]) for attr in attrs]
-        row += pad_list(get_labels(repository, issue), max_num_labels, '')
-        row += [comment['body'] for comment in get_comments(repository, issue)]
-        # As per http://docs.python.org/library/csv.html
+        print "Processing issue [%d/%d]..." % (issue_idx, len(issues))
+        issue_idx += 1
+
+        row = [issue[header] for header in single_field_headers]
+        row += pad_list(get_labels(issue), max_num_labels, '')
+        row += pad_list(issue['Watchers'], max_num_watchers, '')
+        comments = [[comment['Comment Body']] for comment in get_comments(issue)]
+        row += pad_list(list(itertools.chain(*comments)), max_num_comments, '')
         row = [ensure_encoded(e, 'utf-8') for e in row]
         issue_writer.writerow(row)
 
+
 if __name__ == '__main__':
-    with open(sys.argv[2], 'w') as fd:
-        write_jira_csv(fd, sys.argv[1])
+    with open(sys.argv[1], 'w') as fd:
+        write_jira_csv(fd)
